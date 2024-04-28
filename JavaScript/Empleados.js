0927bfdcd8037ea874bbc2db61d6bd49199f2c7c
FelipeAF420 <felix5503@hotmail.com>
2024-04-28 01:39:28 -0600

    Horarios empleados

--- JavaScript/Empleados.js
+++ JavaScript/Empleados.js
@@ -267,13 +267,14 @@ $(document).ready(function() {
         // Manejar el envío del formulario
         $('#formularioRegistroEmpleado').submit(function(event) {
             // Verificar si no se han seleccionado servicios
-            enviarHorario();
             if (serviciosSeleccionados.length === 0) {
                 // Mostrar un mensaje de error al usuario
                 alert("Debes seleccionar al menos un servicio.");
                 // Evitar que se envíen los datos del formulario
                 event.preventDefault();
+                return;
             }
+            // Verificar si se han definido horarios
             if (Object.keys(horariosSemanales).length === 0) {
                 // Mostrar un mensaje de error al usuario
                 alert("Debes agregar al menos un horario.");
@@ -281,18 +282,22 @@ $(document).ready(function() {
                 event.preventDefault();
                 return;
             }
-    
-            // Si se han seleccionado servicios y se han definido horarios, continuar con el envío del formulario
-
+            // Si se han seleccionado servicios y se han definido horarios, validar los horarios antes de enviar el formulario
+            if (!enviarHorario()) {
+                // Si la función enviarHorario() retorna false, significa que hubo un error en la validación
+                // Evitar que se envíen los datos del formulario
+                event.preventDefault();
+                return;
+            }
+            // Si todo está correcto, continuar con el envío del formulario
         });
     });
     
+    
 });
 
-
-// Función para cargar y mostrar los empleados en cards
 $(document).ready(function() {
-    // Función para cargar los empleados
+    // Función para cargar los empleados y sus horarios
     function cargarEmpleados() {
         $.ajax({
             url: '../PHP/ObtenerEmpleado.php',
@@ -301,24 +306,24 @@ $(document).ready(function() {
             success: function(empleados) {
                 // Limpiar el contenedor de empleados antes de agregar nuevos empleados
                 $('.scroll-containerEmpleados').empty();
-    
+
                 // Iterar sobre los empleados recibidos
                 $.each(empleados, function(index, empleado) {
                     // Crear el card del empleado
                     var empleadoCard = $('<div class="card-empleado"></div>');
-    
+
                     // Agregar la imagen del empleado al card
                     var imagenEmpleado = $('<img class="imagen-empleado">');
                     imagenEmpleado.attr('src', empleado.imagen);
                     empleadoCard.append(imagenEmpleado);
-    
+
                     // Agregar el nombre y apellidos del empleado al card
                     var nombreCompleto = empleado.nombre + ' ' + empleado.apellidoP + ' ' + empleado.apellidoM;
                     empleadoCard.append('<h3>' + nombreCompleto + '</h3>');
-    
+
                     // Crear un contenedor para los servicios del empleado
                     var serviciosContainer = $('<div class="scroll-containerServicios"></div>');
-    
+
                     // Realizar una solicitud AJAX para obtener los servicios del empleado
                     $.ajax({
                         url: '../PHP/ObtenerServicioEmpleado.php?idEmpleado=' + empleado.id_empleado,
@@ -329,15 +334,15 @@ $(document).ready(function() {
                             $.each(servicios, function(index, servicio) {
                                 // Crear el card del servicio
                                 var servicioCard = $('<div class="card-servicioEmpleado"></div>');
-    
+
                                 // Agregar la imagen del servicio al card
                                 var imagenServicio = $('<img class="imagen-servicio">');
                                 imagenServicio.attr('src', servicio.imagen);
                                 servicioCard.append(imagenServicio);
-    
+
                                 // Agregar el nombre del servicio al card
                                 servicioCard.append('<h4>' + servicio.nombre + '</h4>');
-    
+
                                 // Agregar el card del servicio al contenedor de servicios del empleado
                                 serviciosContainer.append(servicioCard);
                             });
@@ -346,30 +351,69 @@ $(document).ready(function() {
                             console.error('Error al obtener los servicios del empleado:', error);
                         }
                     });
-    
+
                     // Agregar el contenedor de servicios al card del empleado
                     empleadoCard.append(serviciosContainer);
-    
-                    // Crear el botón de menú desplegable
-                    var dropdownBtn = $('<div class="dropdown-btn">⋮</div>');
-    
-                    // Crear el contenido del menú desplegable
-                    // Agregar el contenido del menú desplegable al botón con un ID que almacene el ID del empleado
-                    var dropdownContent = $('<div class="dropdown-content">' +
-                    '<a href="#" class="editar-empleado" data-id="' + empleado.id_empleado + '">Editar</a>' +
-                    '<a href="#" class="eliminar-empleado" data-id="' + empleado.id_empleado + '">Eliminar</a>' +
-                    '</div>');
 
-    
-                    // Agregar el contenido del menú desplegable al botón
-                    dropdownBtn.append(dropdownContent);
-    
-                    // Agregar el botón de menú desplegable al card del empleado
-                    empleadoCard.append(dropdownBtn);
-    
+                    // Crear un select para los días de la semana
+                    var selectDias = $('<select class="select-dias-semana"></select>');
+                    selectDias.append('<option value="Lunes">Lunes</option>');
+                    selectDias.append('<option value="Martes">Martes</option>');
+                    selectDias.append('<option value="Miércoles">Miércoles</option>');
+                    selectDias.append('<option value="Jueves">Jueves</option>');
+                    selectDias.append('<option value="Viernes">Viernes</option>');
+                    selectDias.append('<option value="Sábado">Sábado</option>');
+                    selectDias.append('<option value="Domingo">Domingo</option>');
+
+                    // Agregar el select al card del empleado
+                    empleadoCard.append(selectDias);
+
+                    // Crear el contenedor para mostrar los horarios
+                    var horariosContainer = $('<div class="horarios-empleado"></div>');
+
+                    // Agregar el contenedor de horarios al card del empleado
+                    empleadoCard.append(horariosContainer);
+
+                    // Evento change para el select de días de la semana
+                   // Evento change para el select de días de la semana
+                    selectDias.on('change', function() {
+                        var diaSeleccionado = $(this).val();
+                        horariosContainer.empty();
+
+                        // Realizar una solicitud AJAX para obtener los horarios del empleado para el día seleccionado
+                        $.ajax({
+                            url: '../PHP/RecuperarHorarioEmpleado.php?idEmpleado=' + empleado.id_empleado + '&diaSemana=' + diaSeleccionado,
+                            method: 'GET',
+                            dataType: 'json',
+                            success: function(horario) {
+                                // Verificar si se encontró un horario para el día seleccionado
+                                if (horario) {
+                                    // Crear el card del horario
+                                    var horarioCard = $('<div class="card-horarioEmpleado"></div>');
+                                    horarioCard.append('<p>Día: ' + horario.dia_semana + '</p>');
+                                    horarioCard.append('<p>Hora de inicio: ' + horario.hora_inicio + '</p>');
+                                    horarioCard.append('<p>Hora de fin: ' + horario.hora_fin + '</p>');
+                                    horarioCard.append('<p>Hora de descanso inicio: ' + horario.hora_descanso_inicio + '</p>');
+                                    horarioCard.append('<p>Hora de descanso fin: ' + horario.hora_descanso_fin + '</p>');
+                                    horariosContainer.append(horarioCard);
+                                } else {
+                                    // Si no se encontró un horario para el día seleccionado, mostrar un mensaje
+                                    horariosContainer.append('<p>No se encontró horario para este día</p>');
+                                }
+                            },
+                            error: function(xhr, status, error) {
+                                console.error('No para el día seleccionado:', error);
+                            }
+                        });
+                    });
+
+
                     // Agregar el card del empleado al contenedor de empleados
                     $('.scroll-containerEmpleados').append(empleadoCard);
-    
+
+                    // Cargar los horarios para el primer día seleccionado por defecto
+                    var primerDiaSeleccionado = selectDias.val();
+                    selectDias.trigger('change');
                 });
             },
             error: function(xhr, status, error) {
@@ -377,13 +421,10 @@ $(document).ready(function() {
             }
         });
     }
-    
 
     // Cargar los empleados al cargar la página
     cargarEmpleados();
 });
-
-
 
 $(document).ready(function() {
     // Evento de clic para el botón de eliminar empleado utilizando delegación de eventos
@@ -446,12 +487,11 @@ function eliminarHorarioDelArreglo(diaSemana) {
 
 // Arreglo para almacenar los horarios de trabajo
 var horariosSemanales = {};
-
+var opcionSeleccionadaAnterior = $('#diaSemana').val();
 
 // Función para capturar los horarios de trabajo ingresados
 function enviarHorario() {
-   
-    var diaSemana = document.getElementById("diaSemana").value;
+    var diaSemana = opcionSeleccionadaAnterior;
     var horaInicio = document.getElementById("horaInicio").value;
     var horaFin = document.getElementById("horaFin").value;
     var horaInicioDescanso = document.getElementById("horaIniciodescanso").value;
@@ -488,45 +528,50 @@ function enviarHorario() {
             return false;
         }
     }
-    var todosVacios = !document.getElementById("horaInicio").value &&
-    !document.getElementById("horaFin").value &&
-    !document.getElementById("horaIniciodescanso").value &&
-    !document.getElementById("horaFindescanso").value;
+    var todosVacios = !horaInicio && !horaFin && !horaInicioDescanso && !horaFinDescanso;
 
-// Si todos los campos están vacíos, eliminar el horario del día del arreglo
+    // Si todos los campos están vacíos, eliminar el horario del día del arreglo
     if (todosVacios) {
-    return false
+        delete horariosSemanales[diaSemana];
+    } else {
+        // Almacenar los horarios en el arreglo utilizando el día de la semana como clave
+        var horarios = {
+            "diaSemana": diaSemana,
+            "horaInicio": horaInicio,
+            "horaFin": horaFin,
+            "horaInicioDescanso": horaInicioDescanso,
+            "horaFinDescanso": horaFinDescanso
+        };
+        horariosSemanales[diaSemana] = horarios;
     }
 
-
-    var horarios = {
-        "diaSemana": diaSemana,
-        "horaInicio": horaInicio,
-        "horaFin": horaFin,
-        "horaInicioDescanso": horaInicioDescanso,
-        "horaFinDescanso": horaFinDescanso
-    };
-
-    // Agregar los horarios al arreglo utilizando el día de la semana como clave
-    horariosSemanales[diaSemana] = horarios;
-    
     console.log("Horarios enviados:", JSON.stringify(horariosSemanales));
-
     document.getElementById("horariosSeleccionados").value = JSON.stringify(horariosSemanales);
 
-
-    // Si todas las validaciones pasan, devuelve true para enviar el formulario
+    // Si todas las validaciones pasan, devuelve true
     return true;
 }
 
 
-// Evento change para el select de día de la semana
+
 $('#diaSemana').change(function() {
+
+    var opcionSeleccionadaActual = $(this).val();
+
     // Llamar a la función enviarHorario() para capturar los horarios de trabajo al cambiar la opción
-   
-    
+    var resultado = enviarHorario();
+
+    // Si alguna condición en enviarHorario() requiere que no se cambie la opción seleccionada, restaurarla
+    if (!resultado) {
+        $('#diaSemana').val(opcionSeleccionadaAnterior);
+    } else {
+        // Actualizar el valor de la opción seleccionada anterior
+        opcionSeleccionadaAnterior = opcionSeleccionadaActual;
+    }
+
     var diaSeleccionado = this.value;
     var horarios = horariosSemanales[diaSeleccionado];
+
     if (horarios) {
         // Si existen horarios definidos para el día seleccionado, actualizar los campos de entrada de tiempo
         $('#horaInicio').val(horarios.horaInicio);
@@ -540,7 +585,5 @@ $('#diaSemana').change(function() {
         $('#horaIniciodescanso').val('');
         $('#horaFindescanso').val('');
     }
-    enviarHorario();
+
 });
-
-
